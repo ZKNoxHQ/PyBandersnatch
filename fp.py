0a7ebba1b9bd0f067defca41c96f93ffc19cef6e
Simon Masson <simon.masson@yahoo.fr>
2024-11-09 12:31:16 +0100

    Element structure

--- fp.py
+++ fp.py
@@ -1,5 +1,5 @@
 import unittest
-from gmpy2 import random_state, mpz_random, invert, mpz, powmod
+from gmpy2 import random_state, mpz_random, invert, mpz, powmod, sign, mod
 
 class Fp():
     
@@ -14,73 +14,113 @@ class Fp():
         self.two_adicity -= 1
 
         # Quadratic non-residue
-        self.non_square = mpz(1)
-        while self.is_square(self.non_square):
-            self.non_square *= -1
-            if self.non_square > 0:
-                self.non_square += 1
+        self.non_square = self.Element(mpz(1), self)
+        while self.non_square.is_square():
+            self.non_square += self.Element(1, self)
 
     def __str__(self):
-        return "Finite field of characteristic {}".format(p)
+        return "Finite field of characteristic {}".format(self.p)
 
+    def __call__(self, value):
+        return self.Element(value, self)
+    
     def random(self):
         # probably not secure
-        return mpz_random(self._rand_state, self.p)
+        return self.Element(mpz_random(self._rand_state, self.p), self)
+                
+
+
+    class Element:
+        def __init__(self, value, field):
+            self.value = value % field.p
+            self.field = field
         
-    def is_square(self,x):
-        # return 0 if x=0, 1 if x is a non-zero square mod p, and -1 if it is not a square
-        # TODO optimize the case (p,q) = (q,p) * something
-        if x==0:
-            return True
-        else:
-            return powmod(mpz(x),(self.p-1)//2, self.p) == 1
-    
-    def mul(self, a, b):
-        return (a*b)%self.p
-    
-    def square(self, a):
-        return self.mul(a,a) # TODO can be more efficient ?
-    
-    def sqrt(self, x):
-        # Return the square root (mod p) or None if no solution exists
-        # from https://www.lvzl.fr/teaching/2020-21/docs/AA-devoir-1-sujet.pdf
-        # and https://en.wikipedia.org/wiki/Tonelli%E2%80%93Shanks_algorithm
-        if x == 0 :
-            return mpz(0)
-        if self.p % 4 == 3:
-            # TODO ASSERT IS SQUARE BEFORE RETURN
-            return powmod(mpz(x),(self.p+1)//4, self.p)
-        # Tonelli-Shanks
-        z = self.non_square
-        M = self.two_adicity
-        Q = (self.p-1)//2**M
+        def __eq__(self,other):
+            if isinstance(other, self.field.Element) and self.field == other.field:
+                return self.value == other.value 
+            raise TypeError("Cannot add elements from different fields.")
+        
+        def __add__(self, other):
+            if isinstance(other, self.field.Element) and self.field == other.field:
+                return self.field(self.value + other.value) # TODO SOMETIMES DO `% self.field.p`
+            raise TypeError("Cannot add elements from different fields.")
 
-        c = powmod(z, Q, self.p)
-        t = powmod(mpz(x), Q, self.p)
-        r = powmod(mpz(x), (Q+1)//2, self.p)
-        # TODO REMOVE THIS CHECK
-        assert(powmod(t, 1<<self.two_adicity,self.p) == 1)
+        def __neg__(self):
+            return self.field(-self.value % self.field.p)
+        
+        def __sub__(self, other):
+            if isinstance(other, self.field.Element) and self.field == other.field:
+                return self.field(self.value - other.value) # TODO SOMETIMES DO `% self.field.p)`
+            raise TypeError("Cannot subtract elements from different fields.")
 
-        while t != 1:
-            # Find the least i (0 < i < m) such that t^(2^i) ≡ 1 (mod p)
-            t2i = t
-            i = 0
-            while t2i != 1:
-                t2i = self.square(t2i)
-                i+=1
-            # Update variables for the next iteration
-            b = powmod(c, 1<<(M - i - 1), self.p)
-            M = i
-            c = self.square(b)
-            t = self.mul(t,c)
-            r = self.mul(r,b)
+        def __mul__(self, other):
+            if isinstance(other, self.field.Element) and self.field == other.field:
+                return self.field((self.value * other.value) % self.field.p)
+            raise TypeError("Cannot multiply elements from different fields.")
+
+        def __pow__(self, exponent):
+            # Raise the element to the power of `exponent` modulo `modulus`
+            result = powmod(self.value, exponent, self.field.p)
+            return self.field(result)
+
+        def is_square(self):
+            # return 0 if x=0, 1 if x is a non-zero square mod p, and -1 if it is not a square
+            # TODO optimize the case (p,q) = (q,p) * something
+            if self.value==0:
+                return True
+            else:
+                return self ** ((self.field.p-1)>>1) == self.field(1)
+
+        def __truediv__(self, other):
+            if isinstance(other, self.field.Element) and self.field == other.field:
+                inverse_other = invert(other.value, self.field.p)
+                return self.field((self.value * inverse_other) % self.field.p)
+            # TODO division by zero error?
+            raise TypeError("Cannot divide elements from different fields.")
+
+        def __repr__(self):
+            return f"{self.value}"
+    
+        def sqrt(self):
+            # Return the square root (mod p) or None if no solution exists
+            # from https://www.lvzl.fr/teaching/2020-21/docs/AA-devoir-1-sujet.pdf
+            # and https://en.wikipedia.org/wiki/Tonelli%E2%80%93Shanks_algorithm
+
+            if self.value == 0 :
+                return self
             
-        return r if t == 1 else None
+            if self.field.p % 4 == 3:
+                # TODO ASSERT IS SQUARE BEFORE RETURN
+                return powmod(self.value,(self.field.p+1)//4, self.field.p)
+            
+            # Tonelli-Shanks
+            z = self.field.non_square
+            M = self.field.two_adicity
+            Q = (self.field.p-1)//2**M
+
+            c = z**Q
+            t = self**Q
+            r = self**((Q+1)>>1)
+            # TODO REMOVE THIS CHECK
+            assert(t**(1<<self.field.two_adicity) == self.field(1))
+
+            while t != self.field(1):
+                # Find the least i (0 < i < m) such that t^(2^i) ≡ 1 (mod p)
+                t2i = t
+                i = 0
+                while t2i != self.field(1):
+                    t2i = t2i**2
+                    i+=1
+                    
+                # Update variables for the next iteration
+                b = c**(1<<(M-i-1))
+                M = i
+                c = b*b
+                t = t*c
+                r = r*b
+                
+            return r if t == self.field(1) else None
     
-    def div(self, x, y):
-        x = mpz(x)
-        y = mpz(y)
-        return self.mul(x,invert(y, self.p))
 
 class TestFp(unittest.TestCase, Fp):
 
@@ -93,30 +133,33 @@ class TestFp(unittest.TestCase, Fp):
 
     def test_mul(self):
         F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
-        self.assertEqual(6, F.mul(2,3))
+        a = F(12345)
+        b = F(54321)
+        a_mul_b = F(670592745)
+        self.assertEqual(a_mul_b, a*b)
 
     def test_div(self):
         F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
-        x = 1234567890
-        y = 9876543210
-        expected_division = 35275400988426393170843532131382492504976292327056669892310128774672708665364 # from sage
-        self.assertEqual(F.div(x,y), expected_division)
+        a = F(1234567890)
+        b = F(9876543210)
+        a_div_b = F(35275400988426393170843532131382492504976292327056669892310128774672708665364) # from sage
+        self.assertEqual(a/b, a_div_b)
 
     def test_sqrt(self):
         F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
         # find a square
         sq = F.random()
-        while not(F.is_square(sq)):
+        while not(sq.is_square()):
             sq = F.random()
         # square-root computation
-        root = F.sqrt(sq)
-        self.assertEqual(F.square(root), sq)
+        root = sq.sqrt()#F.sqrt(sq)
+        self.assertEqual(root*root, sq)
 
     def test_is_square(self):
         F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
-        self.assertFalse(F.is_square(F.non_square))
-        for i in range(F.non_square):
-            self.assertTrue(F.is_square(i))
+        self.assertFalse(F.non_square.is_square())
+        for i in range(F.non_square.value):
+            self.assertTrue(F(i).is_square())
         
 if __name__ == '__main__':
     unittest.main()
