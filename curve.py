c043880be4993b7a19dc771f7b8197923c981b73
Simon Masson <simon.masson@yahoo.fr>
2024-11-11 15:27:57 +0100

    Improve the GLV and tests

--- curve.py
+++ curve.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*- 
 import unittest
 from fp import Fp
+from gmpy2 import mpq
 
 class Curve:
     def __init__(self, a, b, r, h):
@@ -129,7 +130,7 @@ class Curve:
         
         def multi_scalar_mul(self,k1,other, k2, other_minus_self):
             # Algorithm 9 of https://eprint.iacr.org/2017/212.pdf
-            s0, s1, P0, P1, Pm = k1, k2, self, other, other_minus_self
+            s0, s1, P0, P1, Pm = abs(k1), abs(k2), self, other, other_minus_self
 
             while s0 != 0:
                 if s1<s0:
@@ -158,9 +159,11 @@ class Curve:
             c = self.curve.a+2#field(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de9a) 
             return self.curve(-(x-z)**2-c*x*z, 2*x*z)
         
-        def φm1(self):
-            # TODO
-            return 1
+        def φ_minus_one(self):
+            α = self.curve.field(13017314467421381532402061398313046228820690393386411611562176812113295071440)
+            β = self.curve.field(14989411347484419666605643019079533103863186413725217032868654387860539633484)
+            γ = self.curve.field(39953720565912266872856944794434720047230584117801669040511822283402326025498)
+            return self.curve(α* self.x * (self.x+ self.z * β)**2, self.z * (self.x + γ * self.z)**2)
         
         def glv(self,k):
             # N0 and det obtained using:
@@ -168,62 +171,86 @@ class Curve:
             # M = Matrix([[-L,1], [r,0]]).LLL()
             # det = det(M)
             # N = det * M.inv()
-            # N0 = [N[0][0], N[1][0]]
+            # N0 = [N[0][0], N[0][1]]
             # ```
-            N0 = [113482231691339203864511368254957623327,21482638764116277775478679919733259912]
+            M1 = [113482231691339203864511368254957623327,   10741319382058138887739339959866629956]
+            M2 = [21482638764116277775478679919733259912, -113482231691339203864511368254957623327]
+            # inverse*det first row
+            N1 = [113482231691339203864511368254957623327, 10741319382058138887739339959866629956]
             det = 13108968793781547619861935127046491459309155893440570251786403306729687672801
-            beta = [N[0] * self.r//det, N[1] * self.r//det]
-            b = [self.curve.r * beta[0], self.curve.r * beta[1]]
-            k1 = self.curve.r-b[0]
-            k2 = -b[1]
-            return self.multi_scalar_mul(k1, self.φ(), k2, self.φm1())
+            b = [round(mpq(k*N1[0],det)), round(mpq(k*N1[1],det))]
+            k1 = k-b[0] * M1[0] - b[1] * M2[0]
+            k2 = -b[0] * M1[1] -b[1] * M2[1]
+            print("k ={}".format(k))
+            print("k1={}".format(k1))
+            print("k2={}".format(k2))
+            return self.multi_scalar_mul(k1, self.φ(), k2, self.φ_minus_one())
 
+class TestCurve(unittest.TestCase):
 
-class TestFp(unittest.TestCase):
-
-    def test_curve(self):
-        # from bandersnatch paper (montgomery)
+    def set_up_curve(self):
+        # bandersnatch curve in montgomery model
         F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
         a = F(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de98)
         b = F(0x300c3385d13bedb7c9e229e185c4ce8b1dd3b71366bb97c30855c0aa41d62727)
         r = 13108968793781547619861935127046491459309155893440570251786403306729687672801 
         h = 4
-        E = Curve(a,b,r,h)
+        return Curve(a, b, r, h)
+    
+    def test_j_invariant(self):
+        E = self.set_up_curve()
         self.assertEqual(E.j_inv(), 8000)
 
     def test_random(self):
-        # from bandersnatch paper (montgomery)
-        F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
-        a = F(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de98)
-        b = F(0x300c3385d13bedb7c9e229e185c4ce8b1dd3b71366bb97c30855c0aa41d62727)
-        r = 13108968793781547619861935127046491459309155893440570251786403306729687672801 
-        h = 4
-        E = Curve(a,b,r,h)
+        E = self.set_up_curve()
         for i in range(10):
-            P = h*E.random()
-            self.assertTrue(P.is_prime_order_point(r))
+            P = E.h*E.random()
+            self.assertTrue(P.is_prime_order_point(E.r))
+
+    def test_φ_norm(self):
+        E = self.set_up_curve()
+        P = E.h*E.random()
+        φ2P = P.φ().φ()
+        self.assertEqual(φ2P, -2*P)
+
+    def test_φ_eigenvalue(self):
+        E = self.set_up_curve()
+        F = E.field
+        xP = F(12082691309842786344904703102623539864205261229594144938888324074174613840300)
+        P = E(xP,1)
+        self.assertTrue(P.is_prime_order_point(E.r))
+        λ = 8913659658109529928382530854484400854125314752504019737736543920008458395397
+        self.assertEqual(P.φ(), λ*P)
 
     def test_φ(self):
-        # from bandersnatch paper (montgomery)
-        F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
-        a = F(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de98)
-        b = F(0x300c3385d13bedb7c9e229e185c4ce8b1dd3b71366bb97c30855c0aa41d62727)
-        r = 13108968793781547619861935127046491459309155893440570251786403306729687672801 
-        h = 4
-        E = Curve(a,b,r,h)
-        P = h*E.random()
-        φ2P = P.φ().φ()
-        self.assertTrue(φ2P, -2*P)
+        E = self.set_up_curve()
+        F = E.field
+        xP = F(12082691309842786344904703102623539864205261229594144938888324074174613840300)
+        P = E(xP,1)
+        φP = P.φ()
+        self.assertEqual(φP, E(F(11458922273495097112533829568725487588121633355211186266238294705768134023326),1))
+
+    def test_φ_minus_one(self):
+        E = self.set_up_curve()
+        F = E.field
+        xP = F(33458486013926512409896242145198845310109221311939503997839007609527653194379)
+        P = E(xP, 1)
+        φ_minus_one_P = P.φ_minus_one()
+        expected_x = F(24387954571941077226850081312365069165536914211052705257835068138877697985688)
+        self.assertEqual(φ_minus_one_P, E(expected_x, 1))
+
+    def test_φ_minus_one_2(self):
+        E = self.set_up_curve()
+        F = E.field
+        xP = F(12082691309842786344904703102623539864205261229594144938888324074174613840300)
+        P = E(xP, 1)
+        φ_minus_one_P = P.φ_minus_one()
+        expected_x = F(24033430638574135559207661009508680256329241072539898145614847227501177452871)
+        self.assertEqual(φ_minus_one_P, E(expected_x, 1))
 
     def test_add(self):
-        # from bandersnatch paper (montgomery)
-        F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
-        a = F(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de98)
-        b = F(0x300c3385d13bedb7c9e229e185c4ce8b1dd3b71366bb97c30855c0aa41d62727)
-        r = 13108968793781547619861935127046491459309155893440570251786403306729687672801 
-        h = 4
-        E = Curve(a,b,r,h)
-        
+        E = self.set_up_curve()
+        F = E.field
         xP = F(33458486013926512409896242145198845310109221311939503997839007609527653194379)
         xQ = F(28482916637127167529646451618185459303123407338943306674359938301245425470092)
         xPmQ = F(32798664934949466701208494782160952539180648511133632801776750414975815504392)
@@ -236,14 +263,8 @@ class TestFp(unittest.TestCase):
         self.assertEqual(PpQ, E(xPpQ,1))
 
     def test_dbl(self):
-        # from bandersnatch paper (montgomery)
-        F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
-        a = F(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de98)
-        b = F(0x300c3385d13bedb7c9e229e185c4ce8b1dd3b71366bb97c30855c0aa41d62727)
-        r = 13108968793781547619861935127046491459309155893440570251786403306729687672801 
-        h = 4
-        E = Curve(a,b,r,h)
-        
+        E = self.set_up_curve()
+        F = E.field
         xP = F(33458486013926512409896242145198845310109221311939503997839007609527653194379)
         x2P = F(12082691309842786344904703102623539864205261229594144938888324074174613840300)
 
@@ -252,14 +273,8 @@ class TestFp(unittest.TestCase):
         self.assertEqual(Pdbl, E(x2P,1))
 
     def test_scalar_mul(self):
-        # from bandersnatch paper (montgomery)
-        F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
-        a = F(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de98)
-        b = F(0x300c3385d13bedb7c9e229e185c4ce8b1dd3b71366bb97c30855c0aa41d62727)
-        r = 13108968793781547619861935127046491459309155893440570251786403306729687672801 
-        h = 4
-        E = Curve(a,b,r,h)
-        
+        E = self.set_up_curve()
+        F = E.field
         xP = F(33458486013926512409896242145198845310109221311939503997839007609527653194379)
         k = 2345676543234567654
         xkP = F(38783438532518312076050495050645672027147987262410212490358095175313247827184)
@@ -277,14 +292,8 @@ class TestFp(unittest.TestCase):
         self.assertEqual(lQ, E(xlQ, 1))
 
     def test_multi_scalar_mul(self):
-        # from bandersnatch paper (montgomery)
-        F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
-        a = F(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de98)
-        b = F(0x300c3385d13bedb7c9e229e185c4ce8b1dd3b71366bb97c30855c0aa41d62727)
-        r = 13108968793781547619861935127046491459309155893440570251786403306729687672801 
-        h = 4
-        E = Curve(a,b,r,h)
-        
+        E = self.set_up_curve()
+        F = E.field
         xP = F(33458486013926512409896242145198845310109221311939503997839007609527653194379)
         xQ = F(28482916637127167529646451618185459303123407338943306674359938301245425470092)
         xPmQ = F(32798664934949466701208494782160952539180648511133632801776750414975815504392)
@@ -298,22 +307,56 @@ class TestFp(unittest.TestCase):
         k1_p_plus_k2_q = P.multi_scalar_mul(k1,Q,k2, PmQ)
         self.assertEqual(k1_p_plus_k2_q, E(xk1Pk2Q,1))
 
-    def test_glv(self):
-        # from bandersnatch paper (montgomery)
-        F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
-        a = F(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de98)
-        b = F(0x300c3385d13bedb7c9e229e185c4ce8b1dd3b71366bb97c30855c0aa41d62727)
-        r = 13108968793781547619861935127046491459309155893440570251786403306729687672801 
-        h = 4
-        E = Curve(a,b,r,h)
-        
-        xP = F(33458486013926512409896242145198845310109221311939503997839007609527653194379)
+    def test_multi_scalar_mul_2(self):
+        E = self.set_up_curve()
+        F = E.field
+        xP = F(12082691309842786344904703102623539864205261229594144938888324074174613840300)
         P = E(xP,1)
-        k = 234567890987659876987658765 # Maybe we need a larger k to make glv useful
-        xkP = F(38783438532518312076050495050645672027147987262410212490358095175313247827184)
+        Q = P.φ()
+        xPmQ = F(24033430638574135559207661009508680256329241072539898145614847227501177452871)
+        PmQ = E(xPmQ,1)
 
-        kP = P.glv(k)
-        self.assertEqual(kP, E(xkP,1))
+        k1 = -45894336995428141233269187797940484884
+        k2 = 8683555061824981937504960049179714114
+        k1_p_plus_k2_q = P.multi_scalar_mul(k1,Q,k2, PmQ)
+        self.assertEqual(k1_p_plus_k2_q, E(F(24025662955270513540262306395182467712802082813859915548603039872586808886210),1))
+
+    # def test_glv(self):
+    #     E = self.set_up_curve()
+    #     F = E.field        
+    #     xP = F(12082691309842786344904703102623539864205261229594144938888324074174613840300)
+    #     xkP =F(24025662955270513540262306395182467712802082813859915548603039872586808886210)
+    #     k = 11997154529596648729624281997554038960651754640906483911385998427296165917073
+
+    #     P = E(xP,1)
+    #     kP = -45894336995428141233269187797940484884*P + 8683555061824981937504960049179714114 * P.φ()
+    #     self.assertEqual(kP, E(xkP,1))
+    
+    # def test_bench(self):
+    #     from time import time
+    #     # from bandersnatch paper (montgomery)
+    #     F = Fp(52435875175126190479447740508185965837690552500527637822603658699938581184513)
+    #     a = F(0x4247698f4e32ad45a293959b4ca17afa4a2d2317e4c6ce5023e1fd63d1b5de98)
+    #     b = F(0x300c3385d13bedb7c9e229e185c4ce8b1dd3b71366bb97c30855c0aa41d62727)
+    #     r = 13108968793781547619861935127046491459309155893440570251786403306729687672801 
+    #     h = 4
+    #     E = Curve(a,b,r,h)
+        
+    #     xP = F(12082691309842786344904703102623539864205261229594144938888324074174613840300)
+    #     P = E(xP,1)
+    #     k = 1310896879378154761986193512704649000000155893440570251786403306729687672801
+
+    #     t = time()
+    #     for i in range(100):
+    #         kP_glv = P.glv(k)
+    #     print(time()-t)
+
+    #     t = time()
+    #     for i in range(100):
+    #         kP_naive = k*P
+    #     print(time()-t)
+    #     self.assertEqual(kP_glv, kP_naive)
+    
 
         
 if __name__ == '__main__':
